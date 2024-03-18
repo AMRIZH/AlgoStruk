c5c93e7713a65c2e87d9fd6f5d013ed1d352e97f
Amri Zadi <114711141+AMRIZH@users.noreply.github.com>
2024-03-18 21:18:50 +0700

    disiniBriv

--- Teori/Tugas3.py
+++ Teori/Tugas3.py
@@ -1,15 +1,17 @@
+
+
 # import numpy as np
 string = "I love to learn python"
 def longest(string):
-  k = 0
-  long = ""
-  pisah = string.split()
-  for i in pisah :
-    if len(i) > k :
-      k = len(i)
-      long = i
-  print ("the longest word is",long)
-  
+  k = 0 # data holder to store len of longest string
+  long = "" # data holder to store the lonest string
+  pisah = string.split() # split the string into list of string
+  for i in pisah : # check every string
+    if len(i) > k : # if string is longer than the previous string
+      k = len(i) # store the len of the string
+      long = i # store the longer the string
+  print ("the longest word is",long) #print the longest string
+
 longest(string)
 
 angka = [1,2,3,4,5,6,8,9,10]
@@ -29,7 +31,7 @@ angka = [1,2,3,4,5,6,8,9,10]
   
 # print(missing2(angka))
 
-def missing3(angka):
+def missing3(angka): 
   return 55 - sum(angka) 
 print(missing3(angka))
 
